b42e6fdec88bb9c225818a088d3b75e2c9d3af95
PrinceNano <cnanoko@gmail.com>
2022-10-11 15:36:36 +0200

    Lacking protection to friends firing

---
+++ Assets/Scripts Players/UserMe.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using TMPro;
+
+public class LifeManager : MonoBehaviour
+{
+    public int lifeNumber = 10;
+    public bool isDead = false;
+    GameObject player;
+    public List<string> allTags = new List<string>();
+    public TextMeshProUGUI displayLife;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+    private void OnTriggerEnter(Collider bullet)
+    {
+        foreach(string tag in allTags)
+        {
+            if (bullet.tag == tag)
+            {
+                print(bullet.transform.parent.gameObject.layer);
+                if(bullet.transform.parent.gameObject.layer == 6)
+                {
+                    lifeNumber = lifeNumber - 1;
+                    displayLife.text = lifeNumber.ToString();
+                }
+                
+
+
+            }
+        }
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (lifeNumber == 0) { isDead = true; } else { isDead = false; }
+        if (isDead)
+        {
+            transform.position = new Vector3(2, 2, 2);
+        }
+    }
+}
---
+++ Assets/Scripts Players/displayLife.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public TextMeshProUGUI displayLife;
+    int LifeNum = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
