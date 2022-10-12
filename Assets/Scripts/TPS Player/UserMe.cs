9beb76e93ae6c46bac0da5a15c488219747c3e42
PrinceNano <cnanoko@gmail.com>
2022-10-12 09:37:07 +0200

    reput life

--- Assets/Scripts/TPS Player/UserMe.cs
+++ Assets/Scripts/TPS Player/UserMe.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using TMPro;
 
-public class LifeManager : MonoBehaviour
+public class UserMe : MonoBehaviour
 {
     public int lifeNumber = 10;
     private bool isDead = false;
@@ -12,29 +11,30 @@ public class LifeManager : MonoBehaviour
     GameObject player;
     public List<string> allTags = new List<string>();
     public TextMeshProUGUI displayLife;
-    // Start is called before the first frame update
+
     void Start()
     {
+        displayLife.text = lifeNumber.ToString();
     }
+
     private void OnTriggerEnter(Collider bullet)
     {
-        foreach(string tag in allTags)
+        foreach (string tag in allTags)
         {
-            if (bullet.tag == tag)
+            if (bullet.tag == tag) //Il faut vérifier le layer
             {
                 print(bullet.transform.parent.gameObject.layer);
-                if(bullet.transform.parent.gameObject.layer == 6)
+                if (bullet.transform.parent.gameObject.layer == 6)
                 {
                     lifeNumber = lifeNumber - 1;
                     displayLife.text = lifeNumber.ToString();
                 }
-                
-
 
             }
         }
-        
+
     }
+
     // Update is called once per frame
     void Update()
     {
