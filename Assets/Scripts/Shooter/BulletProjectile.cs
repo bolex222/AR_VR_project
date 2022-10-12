09498dd89c3cfcb7409539827c3f57b6885f426b
Jcrosby.nauja <jeremy.crosby@viacesi.fr>
2022-10-12 15:27:06 +0200

    Add: Projectile & Gun Model & Aim Animation

---
+++ Assets/Scripts/Shooter/BulletProjectile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletProjectile : MonoBehaviour
+{
+    //[SerializeField] private Transform vfxAnimatedHit3;
+    //[SerializeField] private Transform vfxAnimatedHit1;
+
+    private Rigidbody bulletRigidbody;
+
+    private void Awake()
+    {
+        bulletRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        float speed = 40f;
+        bulletRigidbody.velocity = transform.forward * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<BulletTarget>() != null)
+        {
+            Debug.Log("Hit target");
+        } else
+        {
+            Debug.Log("Hit something else");
+        }
+        Destroy(gameObject);
+    }
+}
