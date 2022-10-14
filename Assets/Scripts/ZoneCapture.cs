0c1860906cf679fa61d39fe09e9e13ec126a45a9
Zar3k <adrien.salgon@viacesi.fr>
2022-10-14 16:45:35 +0200

    Up: local team capture

--- Assets/Scripts/ZoneCapture.cs
+++ Assets/Scripts/ZoneCapture.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class ZoneCapture : MonoBehaviour
 {
-    private List<PlayerMapAreas> playerMapAreasList = new List<PlayerMapAreas>();
-    private List<PlayerMapAreas> playerMapAreasListTeamA = new List<PlayerMapAreas>();
-    private List<PlayerMapAreas> playerMapAreasListTeamB = new List<PlayerMapAreas>();
+    private List<PlayerMapAreas> playerMapAreasList;
+    private List<PlayerMapAreas> playerMapAreasListTeamA;
+    private List<PlayerMapAreas> playerMapAreasListTeamB;
 
     public enum State {Neutral, Captured};
     private State state;
@@ -20,6 +20,9 @@ public class ZoneCapture : MonoBehaviour
     void Awake()
     {
         state = State.Neutral;
+        playerMapAreasList = new List<PlayerMapAreas>();
+        playerMapAreasListTeamA = new List<PlayerMapAreas>();
+        playerMapAreasListTeamB = new List<PlayerMapAreas>();
     }
 
     // Update is called once per frame
@@ -34,6 +37,7 @@ public class ZoneCapture : MonoBehaviour
 
                 foreach (PlayerMapAreas playerMapAreas in GetPlayerMapAreas())
                 {
+                    
                     if (!playerMapAreasList.Contains(playerMapAreas))
                     {
                         playerMapAreasList.Add(playerMapAreas);
@@ -46,25 +50,64 @@ public class ZoneCapture : MonoBehaviour
 
                         }
                     }
+                    else if (playerMapAreasList.Contains(playerMapAreas))
+                    {
+                        Debug.Log("already contains");
+                        if (playerMapAreas.gameObject.layer == LayerMask.NameToLayer("TeamA") && !playerMapAreasListTeamA.Contains(playerMapAreas))
+                        {
+                            playerMapAreasListTeamA.Add(playerMapAreas);
+                            Debug.Log("Players A inside: " + playerMapAreasListTeamA.Count);
+
+                        }
+                        else if (playerMapAreas.gameObject.layer == LayerMask.NameToLayer("TeamB") && !playerMapAreasListTeamB.Contains(playerMapAreas))
+                        {
+                            playerMapAreasListTeamB.Add(playerMapAreas);
+                            Debug.Log("Players B inside: " + playerMapAreasListTeamB.Count);
+
+                        }
+                    }
                 }
 
+                if(playerMapAreasListTeamA.Count > 0)
+                {
+                    float progressSpeed = 0.5f;
+                    progress_A += playerMapAreasListTeamA.Count * progressSpeed * Time.deltaTime;
+
+                    Debug.Log("Inside zone: " + playerMapAreasListTeamA.Count + " ; progress A: " + progress_A);
+
+                    if (progress_A >= 1f)
+                    {
+                        state = State.Captured;
+                        GetComponent<Renderer>().material.color = Color.red;
+                    }
+
+                    if (progress_A >= 0 && playerMapAreasListTeamA.Count == 0)
+                    {
+                        progress_A -= progressSpeed * Time.deltaTime;
+                        Debug.Log("Leaved during progress, Player inside zone: " + playerMapAreasListTeamA.Count + " ; progress: " + progress_A);
+                    }
+                }
+
+                else if(playerMapAreasListTeamB.Count > 0)
+                {
+                    float progressSpeed = 0.5f;
+                    progress_B += playerMapAreasListTeamB.Count * progressSpeed * Time.deltaTime;
+
+                    Debug.Log("Inside zone: " + playerMapAreasListTeamB.Count + " ; progress B: " + progress_B);
+
+                    if (progress_B >= 1f)
+                    {
+                        state = State.Captured;
+                        GetComponent<Renderer>().material.color = Color.blue;
+                    }
+
+                    if (progress_B >= 0 && playerMapAreasListTeamB.Count == 0)
+                    {
+                        progress_B -= progressSpeed * Time.deltaTime;
+                        Debug.Log("Leaved during progress, Player inside zone: " + playerMapAreasListTeamB.Count + " ; progress: " + progress_B);
+                    }
+                }
                 
-                float progressSpeed = 0.5f;
-                progress_A += playerMapAreasListTeamA.Count * progressSpeed * Time.deltaTime;
-
-                Debug.Log("Inside zone: " + playerMapAreasListTeamA.Count + " ; progress A: " + progress_A);
-
-                if (progress_A >= 1f)
-                {
-                    state = State.Captured;
-                    GetComponent<Renderer>().material.color = Color.red;
-                }
-
-                 if (progress_A >= 0 && playerMapAreasListTeamA.Count == 0)
-                {
-                    progress_A -= progressSpeed * Time.deltaTime;
-                    Debug.Log("Leaved during progress, Player inside zone: " + playerMapAreasListTeamA.Count + " ; progress: " + progress_A);
-                }
                 
                 break;
             case State.Captured:
