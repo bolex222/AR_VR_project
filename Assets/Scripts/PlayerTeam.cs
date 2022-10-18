28c02e5e0f0861f2287035771b0158436d150438
Basile LECOUTURIER <biblex1222@gmail.com>
2022-10-18 10:51:12 +0200

    teleportation and matchmaking working

--- Assets/Scripts/PlayerTeam.cs
+++ Assets/Scripts/PlayerTeam.cs
@@ -1,9 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using Interfaces;
+using Photon.Pun;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class PlayerTeam : MonoBehaviour
+public class PlayerTeam : MonoBehaviourPunCallbacks
 {
     [SerializeField] public AllGenericTypes.Team team;
+    [SerializeField] private TeleportationProvider teleprovider;
+
+    private void Start()
+    {
+        var teleArea = GameObject.Find("floor").GetComponent<TeleportationArea>();
+        
+        if (photonView.IsMine && teleArea is not null)
+        {
+            teleArea.teleportationProvider = teleprovider;
+        }
+    }
 }
