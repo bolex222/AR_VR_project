50cdc0d53bffbd2c17bd5d80312a2b8eed020081
Jcrosby.nauja <jeremy.crosby@viacesi.fr>
2022-10-18 16:56:52 +0200

    Remove: main menu button

--- Assets/UI documents/GameEndUIManager.cs
+++ Assets/UI documents/GameEndUIManager.cs
@@ -11,7 +11,6 @@ public class GameEndUIManager : MonoBehaviour
     private Label _winnerTeam;
     private Label _draw;
     private Button _playAgainButton;
-    private Button _mainMenuButton;
 
     private VisualElement _gameEndUIElement;
 
@@ -19,22 +18,15 @@ public class GameEndUIManager : MonoBehaviour
     {
         _gameEndUIElement = uiDocument.rootVisualElement;
         _playAgainButton = _gameEndUIElement.Q<Button>("play-again-button");
-        _mainMenuButton = _gameEndUIElement.Q<Button>("main-menu-button");
         _winnerTeam = _gameEndUIElement.Q<Label>("winner-team");
         _draw = _gameEndUIElement.Q<Label>("draw");
 
         _playAgainButton.clicked += PlayAgainButtonPressed;
-        _mainMenuButton.clicked += MainMenuButtonPressed;
     }
 
     private void PlayAgainButtonPressed()
     {
         SceneManager.LoadScene("Matchmaking");
-    }
-
-    private void MainMenuButtonPressed()
-    {
-        SceneManager.LoadScene("Lobby");
     }
 
     private void OnEnable()
