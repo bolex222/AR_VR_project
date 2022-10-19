623e98da715f12ba655708c4721e705a0343ec72
Jcrosby.nauja <jeremy.crosby@viacesi.fr>
2022-10-19 17:25:18 +0200

    Fix: forgot something

--- Assets/RegisterOriginLocation.cs
+++ Assets/RegisterOriginLocation.cs
@@ -8,8 +8,6 @@ public class RegisterOriginLocation : MonoBehaviour
 {
    [SerializeField] private Camera Head;
    [SerializeField] bool rotation;
-   [SerializeField] private Camera MiniMapCamera;
-   [SerializeField] bool miniRotation;
 
     private void Update()
    {
@@ -18,13 +16,6 @@ public class RegisterOriginLocation : MonoBehaviour
       if (rotation)
       {
          transform.rotation = Quaternion.Euler(new Vector3(0, Head.gameObject.transform.rotation.y, 0));
-      }
-
-      transform.position = MiniMapCamera.gameObject.transform.position;
-
-      if (miniRotation)
-      {
-         transform.rotation = Quaternion.Euler(new Vector3(0, MiniMapCamera.gameObject.transform.rotation.y, 0));
       }
     }
 }
